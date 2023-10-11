37f12c0c6e1ea1c5b87e167a2cb9ac0ac72664c7
Gabe Cook <gabe565@gmail.com>
2023-10-10 22:58:31 -0500

    feat: Initial commit

---
+++ gen.go
@@ -0,0 +1,5 @@
+package spinner
+
+//go:generate npm --prefix=internal/generate --no-audit --no-fund install
+
+//go:generate go run -tags=generate ./internal/generate
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gabe565/go-spinners
+
+go 1.21
+
+require github.com/dave/jennifer v1.7.0
