73237a1b5d978aef7fb2b2c71df6287481308413
Gabe Cook <gabe565@gmail.com>
2023-10-11 00:32:25 -0500

    docs(spinner): Add Spinner docs

--- spinner.go
+++ spinner.go
@@ -2,7 +2,10 @@ package spinner
 
 import "time"
 
+// Spinner defines a command-line spinner
 type Spinner struct {
-	Frames   []string
+	// Frames is a list of frames for a spinner.
+	Frames []string
+	// Interval is the recommended interval for a spinner.
 	Interval time.Duration
 }
