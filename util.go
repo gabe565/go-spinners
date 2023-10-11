be8dfd65360cdc7efae0458d685f1f09daff8630
Gabe Cook <gabe565@gmail.com>
2023-10-11 02:16:04 -0500

    fix(util): Fix original frames being modified in util funcs

--- util.go
+++ util.go
@@ -2,10 +2,12 @@ package spinner
 
 // ReverseFrames makes the spinner play backwards.
 func ReverseFrames[T any](frames []T) []T {
-	for i := 0; i < len(frames)/2; i += 1 {
-		frames[i], frames[len(frames)-1-i] = frames[len(frames)-1-i], frames[i]
+	result := make([]T, len(frames))
+	copy(result, frames)
+	for i := 0; i < len(result)/2; i += 1 {
+		result[i], result[len(frames)-1-i] = result[len(frames)-1-i], result[i]
 	}
-	return frames
+	return result
 }
 
 // BoomerangFrames makes the spinner loop back and forth.
@@ -13,8 +15,10 @@ func BoomerangFrames[T any](frames []T) []T {
 	if len(frames) < 3 {
 		return frames
 	}
-	for i := len(frames) - 2; i > 0; i -= 1 {
-		frames = append(frames, frames[i])
+	result := make([]T, len(frames))
+	copy(result, frames)
+	for i := len(result) - 2; i > 0; i -= 1 {
+		result = append(result, result[i])
 	}
-	return frames
+	return result
 }
