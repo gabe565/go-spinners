90a8c96a54e900dad8d112b46e996437c5947ba9
Gabe Cook <gabe565@gmail.com>
2024-04-20 02:00:31 -0500

    chore(generate): Remove build tag

--- gen.go
+++ gen.go
@@ -2,4 +2,4 @@ package spinner
 
 //go:generate npm --prefix=internal/generate --no-audit --no-fund install
 
-//go:generate go run -tags=generate ./internal/generate
+//go:generate go run ./internal/generate
--- internal/generate/main.go
+++ internal/generate/main.go
@@ -1,5 +1,3 @@
-//go:build generate
-
 package main
 
 import (
